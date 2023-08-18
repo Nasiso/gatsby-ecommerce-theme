3c38c143caa4b1d35436576706d7ff9b72a04b99
Nasiso <122862929+Nasiso@users.noreply.github.com>
2023-08-18 16:15:26 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Your one stop shop'}
+        subtitle={'Discover our range of exciting products'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
